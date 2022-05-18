a89a371a3b4cee8d0b544486a2b135037bd4c73e
Juliana <julianaacademi@gmail.com>
2022-05-18 14:58:29 -0300

    utilizado o recurso scaffolde
    que cria os arquivos CRUD

--- RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/RazorPagesMovie/Models/Movie.cs
@@ -4,9 +4,11 @@ namespace RazorPagesMovie.Models
 {
     public class Movie
     {
+        //campo ID é necessário para o banco de dados como chave primária
         public int ID { get; set; }
         public string Title { get; set; } = string.Empty;
 
+        //DATATYPE especifica o tipo de dados
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; } = string.Empty;
