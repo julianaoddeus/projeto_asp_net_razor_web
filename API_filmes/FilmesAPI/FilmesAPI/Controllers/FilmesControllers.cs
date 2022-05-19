454c677832c1019093acf64c0881adc70ada3d39
Juliana <julianaacademi@gmail.com>
2022-05-19 17:33:37 -0300

    inclusão de banco de dados

---
+++ API_filmes/FilmesAPI/FilmesAPI/Controllers/FilmesControllers.cs
@@ -0,0 +1,40 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FilmesAPI.Controllers
+{       
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmesControllers : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+
+        [HttpPost]
+        //função de cadastrar os dados no sistema
+        public IActionResult AdicionaFilme([FromBody]Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            return CreatedAtAction(nameof(RecuperaFilmesPorId), new { id = filme.Id }, filme);
+        }
+
+        [HttpGet]   
+        public IActionResult RecuperaFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmesPorId(int id)
+        {
+           Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if(filme != null)
+            {
+                return Ok(filme);
+            }
+            return NotFound();
+        }
+    }
+}
